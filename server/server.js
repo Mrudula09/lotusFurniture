26c0d110de1ae4f534f495b1af6cd513c2af2db6
Mrudula09 <mrudula.nudurupati1997@gmail.com>
2020-12-29 00:01:17 +0530

    added user schema

--- server/server.js
+++ server/server.js
@@ -1,8 +1,26 @@
 const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const User = require('./models/user');
+
+// Environment variables
+const dotenv = require('dotenv');
+dotenv.config()
 
 const app = express();
+
+// Config database
+mongoose.connect(process.env.DATABASE, 
+{ useNewUrlParser: true, useUnifiedTopology: true },
+ (err) => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log('Connected to database');
+    }
+})
 
 // Middlewares
 app.use(morgan('dev'));
@@ -14,7 +32,18 @@ app.get('/', (req, res) => {
 });
 
 app.post('/', (req, res) => {
-    console.log(req.body.name);
+    let user = new User();
+    user.name = req.body.name;
+    user.password = req.body.password;
+    user.email = req.body.email;
+
+    user.save((err) => {
+        if (err) {
+            res.json(err);
+        } else {
+            res.json('Successfully saved user!!')
+        }
+    })
 });
 
 app.listen(3000, (err) => {

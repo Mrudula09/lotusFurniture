e87d5c914bf4a87ef5287cf7c811ad0fb2d36852
Mrudula09 <mrudula.nudurupati1997@gmail.com>
2021-01-05 23:44:29 +0530

    admin pages

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const User = require('./models/user');
 
@@ -23,6 +24,7 @@ mongoose.connect(process.env.DATABASE,
 })
 
 // Middlewares
+app.use(cors());
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
